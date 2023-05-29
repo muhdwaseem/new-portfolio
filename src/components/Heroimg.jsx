5e96207bfa326d2b9994a7c6cb470f8cdea2f812
muhdwaseem <waseemvann@gmail.com>
2023-05-29 21:03:43 +0530

    commit

--- src/components/Heroimg.jsx
+++ src/components/Heroimg.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './heroimg.css'
-// import introimg from '../assets/pexels-annam-w-1120162.jpg'
+import introimg from '../assets/pexels-annam-w-1120162.jpg'
 import { Link } from 'react-router-dom'
 
 function Heroimg() {
   return (
     <div className='hero'>
     <div className='mask'>
-      <img className='into-img' src='https://images.unsplash.com/photo-1498049860654-af1a5c566876?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjE0fHx0ZWNobm9sb2d5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt="" srcset="" />
+      <img className='into-img' src={introimg} alt="" srcset="" />
       </div>
       <div className="content">
         <p>hello! </p>
